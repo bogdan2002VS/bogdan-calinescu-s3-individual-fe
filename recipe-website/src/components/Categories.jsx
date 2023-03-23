c4a84af45ee0a5518058bc39800876dd07b9311d
Călinescu,Bogdan B.I.C <i489732@fhict.nl>
2023-03-23 17:58:38 +0100

    changes

--- recipe-website/src/components/Categories.jsx
+++ recipe-website/src/components/Categories.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import API from './API';
-import { Typography, List, ListItem, ListItemText, TextField, Button } from '@mui/material';
+import { Typography, List, ListItem, ListItemText, TextField, Button ,Grid} from '@mui/material';
 import styled from 'styled-components';
+
 
 const CategoryListItem = styled(ListItem)`
   display: flex;
@@ -55,7 +56,7 @@ function Categories() {
         return response;
       }
       return category;
-    }));
+    }))
   }
 
   async function handleDelete(id) {
@@ -68,42 +69,46 @@ function Categories() {
       <Typography variant="h4" component="h1" gutterBottom>
         Categories
       </Typography>
-      <form onSubmit={handleCreate}>
-        <CategoryInput label="Category name" value={name} onChange={event => setName(event.target.value)} />
-        <CategoryInput label="Category description" value={description} onChange={event => setDescription(event.target.value)} />
-        <Button variant="contained" color="primary" type="submit">Create</Button>
-      </form>
-      <List>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <form onSubmit={handleCreate}>
+            <CategoryInput label="Category name" value={name} onChange={event => setName(event.target.value)} />
+            <CategoryInput label="Category description" value={description} onChange={event => setDescription(event.target.value)} />
+            <Button variant="contained" color="primary" type="submit">Create</Button>
+          </form>
+        </Grid>
         {categories.map(category => (
-          <CategoryListItem key={category.id}>
-            <CategoryName primary={category.name} secondary={category.description} />
-            <div>
-              <CategoryInput label="New category name" value={category.newName || ''} onChange={event => {
-                const newName = event.target.value;
-                setCategories(categories => categories.map(c => {
-                  if (c.id === category.id) {
-                    return { ...c, newName };
-                  }
-                  return c;
-                }));
-              }} />
-              <CategoryInput label="New category description" value={category.newDescription || ''} onChange={event => {
-                const newDescription = event.target.value;
-                setCategories(categories => categories.map(c => {
-                  if (c.id === category.id) {
-                    return { ...c, newDescription };
-                  }
-                  return c;
-                }));
-              }} />
-              <UpdateButton variant="contained" color="primary" onClick={() => handleUpdate(category.id, category.newName, category.newDescription)}>Update</UpdateButton>
-              <DeleteButton variant="contained" color="secondary" onClick={() => handleDelete(category.id)}>Delete</DeleteButton>
-            </div>
-          </CategoryListItem>
+          <Grid item xs={12} key={category.id}>
+            <CategoryListItem>
+              <CategoryName primary={category.name} secondary={category.description} />
+              <div>
+                <CategoryInput label="New category name" value={category.newName || ''} onChange={event => {
+                  const newName = event.target.value;
+                  setCategories(categories => categories.map(c => {
+                    if (c.id === category.id) {
+                      return { ...c, newName };
+                    }
+                    return c;
+                  }))
+                }} />
+                <CategoryInput label="New category description" value={category.newDescription || ''} onChange={event => {
+                  const newDescription = event.target.value;
+                  setCategories(categories => categories.map(c => {
+                    if (c.id === category.id) {
+                      return { ...c, newDescription };
+                    }
+                    return c;
+                  }))
+                }} />
+                <UpdateButton variant="contained" color="primary" onClick={() => handleUpdate(category.id, category.newName, category.newDescription)}>Update</UpdateButton>
+                <DeleteButton variant="contained" color="secondary" onClick={() => handleDelete(category.id)}>Delete</DeleteButton>
+              </div>
+            </CategoryListItem>
+          </Grid>
         ))}
-      </List>
+      </Grid>
     </div>
-  );
+  )
 }
 
 export default Categories;
